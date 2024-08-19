7063a6d0f1474ec7510c6c879dc9bd98119c7a4d
vladolexy <perov2199@gmail.com>
2024-08-19 14:19:41 +0300

    react file

---
+++ car-catalog/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './components/screns/home/home.jsx'
+import './index.css'
+import './style/global.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
